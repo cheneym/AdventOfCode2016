7dd7c5732882252265bb8bed5955692674f0b820
Cheney Mao <cheney@Cheneys-MacBook-Pro.local>
2017-08-08 00:23:48 -0700

    Complete day 1 (python)

--- 2015/day01/day01.py
+++ 2015/day01/day01.py
@@ -1,7 +1,28 @@
-import os
+## Part 1
 
 with open('input.txt') as f:
     lines = f.readlines()
 inputData = lines[0]
-inputData = inputData.split(', ')
-print inputData
+
+floor = 0
+for paren in inputData:
+    if paren == '(':
+        floor += 1
+    else:
+        floor -= 1
+
+print floor # 280
+
+
+## Part 2
+floor = 0
+for index, paren in enumerate(inputData):
+    if paren == '(':
+        floor += 1
+    else:
+        floor -= 1
+    if floor == -1:
+        print index + 1
+        break
+
+# 1797
